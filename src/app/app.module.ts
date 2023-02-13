a8f291c92b5d6608ebf520f29f25884ef503d0dd
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-02-13 12:43:27 +0200

    modal opens when button is clicked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientModule } from './client/client.module';
+import { ToggleButtonComponent } from './components/toggle-button/toggle-button.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToggleButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/toggle-button/toggle-button.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-toggle-button',
+  templateUrl: './toggle-button.component.html',
+  styleUrls: ['./toggle-button.component.css']
+})
+export class ToggleButtonComponent implements OnInit {
+
+  constructor(public modal: ModalService) { }
+
+  ngOnInit(): void {
+  }
+
+  openModal($event: Event){
+    $event.preventDefault();
+
+    this.modal.toggleModal();
+  }
+
+}
