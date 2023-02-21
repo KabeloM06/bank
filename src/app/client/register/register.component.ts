6043ddf2ba8af22c6930f219b68ed14c559b42d5
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-02-21 10:15:52 +0200

    form control binded and name input field works

--- src/app/client/register/register.component.ts
+++ src/app/client/register/register.component.ts
@@ -7,16 +7,22 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  name= new FormControl('',[
+    Validators.required,
+    Validators.minLength(3)
+  ])
+    email= new FormControl('')
+  phoneNumber= new FormControl('')
+  id_number= new FormControl('')
+  password= new FormControl('')
+  confirm_password= new FormControl('')
+  
+
   registerForm = new FormGroup({
-    name: new FormControl('',[
-      Validators.required,
-      Validators.minLength(3)
-    ]),
-    email: new FormControl(''),
-    phoneNumber: new FormControl(''),
-    id_number: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    
-  })
-}
+    name: this.name,
+    email: this.email ,
+    phoneNumber: this.phoneNumber,
+    id_number: this.id_number,
+    password: this.password,
+    confirm_password: this.confirm_password 
+})}
