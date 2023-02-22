f214e0b3ca7c0b01a858c3e8b233def98bcefe0c
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-02-22 17:14:53 +0200

    validation of ID number field on register form now use minlength and maxlength

--- src/app/client/register/register.component.ts
+++ src/app/client/register/register.component.ts
@@ -21,8 +21,13 @@ export class RegisterComponent {
     Validators.minLength(13),
     Validators.maxLength(13)
   ])
-  password= new FormControl('')
-  confirm_password= new FormControl('')
+  password= new FormControl('',[
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password= new FormControl('',[
+    Validators.required
+  ])
   
 
   registerForm = new FormGroup({
